dcb5930a1837d032176c88d0e2001d36432a6f1c
Mike Silvis <Mike.Silvis@LivingSocial.com>
2013-02-10 22:34:21 -0500

    adding more states and a custom cell

--- cuddle-app/ContactIndexController.m
+++ cuddle-app/ContactIndexController.m
@@ -7,13 +7,34 @@
 //
 
 #import "ContactIndexController.h"
+#import "ContactInfoCell.h"
+
+@interface ContactIndexController ()
+
+@end
 
 @implementation ContactIndexController
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    NSLog(@"In here");
-    self.navigationItem.hidesBackButton = YES;
+//- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+//    static NSString *cellIdentifier = @"cellIdentifier";
+//    
+//    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+//    if (!cell) {
+//        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:nil];
+//        cell.textLabel.textColor = [UIColor colorWithRed:0.6f green:0.7f blue:0.8f alpha:1.0f];
+//    }
+//    cell.textLabel.text = @"Mike Silvis";
+//    cell.detailTextLabel.text = @"Called 30 days ago";
+//    return cell;
+//}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 5;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    ContactInfoCell *cell = (ContactInfoCell *)[tableView dequeueReusableCellWithIdentifier:@"contactInfoCell"];
+    
+    return cell;
 }
 
 @end
--- cuddle-app/ContactInfoCell.m
+++ cuddle-app/ContactInfoCell.m
@@ -10,4 +10,24 @@
 
 @implementation ContactInfoCell
 
+@synthesize userName;
+@synthesize lastContact;
+@synthesize userPicture;
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        NSLog(@"WOO");
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+    
+    // Configure the view for the selected state
+}
 @end
