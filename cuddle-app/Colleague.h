93cb245294430ec9b84413ad09a069eeb780f570
Paul Silvis <paulsilvis@gmail.com>
2013-02-11 00:15:24 -0800

    adding colleague model

---
+++ cuddle-app/Colleague.h
@@ -0,0 +1,16 @@
+//
+//  Colleague.h
+//  cuddle-app
+//
+//  Created by Paul Silvis on 2/10/13.
+//  Copyright (c) 2013 Mike Silvis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Colleague : NSObject
+
+@property NSString *firstName;
+@property NSString *lastName;
+
+@end
---
+++ cuddle-app/Colleague.m
@@ -0,0 +1,13 @@
+//
+//  Colleague.m
+//  cuddle-app
+//
+//  Created by Paul Silvis on 2/10/13.
+//  Copyright (c) 2013 Mike Silvis. All rights reserved.
+//
+
+#import "Colleague.h"
+
+@implementation Colleague
+
+@end
