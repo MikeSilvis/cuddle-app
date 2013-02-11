082995d5e643a41e42c4618988fa3ae435b22890
Mike Silvis <Mike.Silvis@LivingSocial.com>
2013-02-10 20:01:00 -0500

    adding storyboards

---
+++ cuddle-app/ContactShowViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContactShowViewController.h
+//  cuddle-app
+//
+//  Created by Mike Silvis on 2/10/13.
+//  Copyright (c) 2013 Mike Silvis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactShowViewController : UIViewController
+
+@end
---
+++ cuddle-app/ContactShowViewController.m
@@ -0,0 +1,13 @@
+//
+//  ContactShowViewController.m
+//  cuddle-app
+//
+//  Created by Mike Silvis on 2/10/13.
+//  Copyright (c) 2013 Mike Silvis. All rights reserved.
+//
+
+#import "ContactShowViewController.h"
+
+@implementation ContactShowViewController
+
+@end
